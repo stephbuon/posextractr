11493f604bf6a7bb704766badc99a5f07fb54a98
stephbuon <steph.buon@gmail.com>
2022-05-10 12:23:53 -0500

    added here::

--- R/extract_adj_noun_pairs.R
+++ R/extract_adj_noun_pairs.R
@@ -3,7 +3,7 @@ extract_adj_noun_pairs <- function(hansard) {
 
   reticulate::use_python("/usr/bin/python3")
 
-  path <- here()
+  path <- here::here()
   path <- paste0(path, "/")
 
   t <- reticulate::import_from_path("posextract.driver", path = path, convert = TRUE) #"./posextractr/", convert = TRUE)
