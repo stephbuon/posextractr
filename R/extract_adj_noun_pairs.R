56e0c00feb172f8c0c1f692fa7336d249f5f0620
stephbuon <steph.buon@gmail.com>
2022-05-10 15:33:03 -0500

    test path

--- R/extract_adj_noun_pairs.R
+++ R/extract_adj_noun_pairs.R
@@ -4,10 +4,13 @@ extract_adj_noun_pairs <- function(hansard) {
   reticulate::use_python("/usr/bin/python3")
 
   path <- here::here()
-  path <- paste0(path, "/")
+  path <- paste0(path, "/posextract/")
 
-  t <- reticulate::import_from_path("posextract.driver", path = path, convert = TRUE) #"./posextractr/", convert = TRUE)
+  t <- reticulate::import_from_path("trash.driver", path = "~/projects/", convert = T) #"./posextractr/", convert = TRUE)
 
-  hansard_small <- t$driver(hansard)
 
-  return(hansard_small) }
+  # import_from_path
+  hansard <- t$driver(hansard)
+
+  return(hansard) }
+
