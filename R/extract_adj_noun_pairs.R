0b9812c4113857e02c555851aa4a3a9fc919b598
stephbuon <steph.buon@gmail.com>
2023-01-04 12:49:39 -0600

    Updated DESC

--- R/extract_adj_noun_pairs.R
+++ R/extract_adj_noun_pairs.R
@@ -4,6 +4,7 @@
 #'
 #' @importFrom reticulate import
 #' @importFrom dplyr na_if
+#' @export
 extract_adj_noun_pairs <- function(vect, lemmatize) {
   posextract <- import("posextract")
   dataframe <- posextract$adj_noun_pairs$extract(vect, lemmatize, want_dataframe = TRUE, verbose = FALSE)
