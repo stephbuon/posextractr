ab04351994c78b38c6ec9ee3c6ee85c033738c42
stephbuon <steph.buon@gmail.com>
2023-01-04 13:03:21 -0600

    Updated DESC

--- R/download_spacy_lang_model.R
+++ R/download_spacy_lang_model.R
@@ -3,7 +3,7 @@
 #' Download the small English spaCy langauge model.
 #'
 #' @importFrom reticulate import
-#' @export
+#' @noRd
 download_spacy_lang_model <- function() {
   posextract <- import("posextract")
   posextract$download_language_model$download_en_core_web_sm() }
